45c09fb9e3a53864605b417f96e3c99a098f950b
Taha El Mouatadir <tahelmo@tuta.io>
2022-02-12 22:26:29 -0500

    added simple API endpoint

--- src/main/java/com/latebox/server/simpleObject/SimpleObject.java
+++ src/main/java/com/latebox/server/simpleObject/SimpleObject.java
@@ -1,11 +1,11 @@
-package com.latebox.server;
+package com.latebox.server.simpleObject;
 
-public class Greeting {
+public class SimpleObject {
 
     private final long id;
     private final String content;
 
-    public Greeting(long id, String content) {
+    public SimpleObject(long id, String content) {
         this.id = id;
         this.content = content;
     }
