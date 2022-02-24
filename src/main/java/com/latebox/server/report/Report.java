632c5ffd57ee8258eb33652d2d345c2bf330b6c0
uygaraktuna <43392169+uygaraktuna@users.noreply.github.com>
2022-02-24 12:39:45 -0500

    Added Reports

---
+++ src/main/java/com/latebox/server/report/Report.java
@@ -0,0 +1,69 @@
+package com.latebox.server.report;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Report {
+
+    private @Id
+    @GeneratedValue
+    Long id;
+    private int userId;
+    private int restaurantId;
+    private String reportContent;
+
+
+    Report() {}
+
+    public Report(int userId, int restaurantId, String reportContent) {
+        this.userId = userId;
+        this.restaurantId = restaurantId;
+        this.reportContent = reportContent;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getRestaurantId() {
+        return restaurantId;
+    }
+
+    public void setRestaurantId(int restaurantId) {
+        this.restaurantId = restaurantId;
+    }
+
+    public String getReportContent() {
+        return reportContent;
+    }
+
+    public void setReportContent(String reportContent) {
+        this.reportContent = reportContent;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Report{" +
+                "id=" + id +
+                ", userId='" + userId + '\'' +
+                ", restaurantId='" + restaurantId + '\'' +
+                ", report=" + reportContent +
+                '}';
+    }
+
+}
---
+++ src/main/java/com/latebox/server/report/ReportNotFoundException.java
@@ -0,0 +1,7 @@
+package com.latebox.server.report;
+
+public class ReportNotFoundException extends  RuntimeException {
+    ReportNotFoundException(Long id) {
+        super("Could not find report " + id);
+    }
+}
