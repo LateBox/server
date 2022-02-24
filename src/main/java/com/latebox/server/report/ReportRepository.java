e7f009c0d4c75033b565a08e5d8030df786f2878
uygaraktuna <43392169+uygaraktuna@users.noreply.github.com>
2022-02-24 13:23:30 -0500

    Fixed a bug thanks to Taha

--- src/main/java/com/latebox/server/report/ReportRepository.java
+++ src/main/java/com/latebox/server/report/ReportRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 public interface ReportRepository extends JpaRepository<Report, Long> {
 
 
-    List<Report> findByName(String name);
+//    List<Report> findByName(String name);
 
     Report findById(long id);
 }
